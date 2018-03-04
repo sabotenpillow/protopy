2f6d7023cfcfe14c5dee66a13e9e36c2a52eb806
sabotenpillow <saboten.pillow@gmail.com>
2018-03-04 19:23:40 +0000

    receive raw as bytes type && have val of "Content-Encoding" and "Transfer-Encoding" as list

--- http/http.py
+++ http/http.py
@@ -1,6 +1,8 @@
 import re
+import gzip
 
-CRLF = '\r\n'
+CRLF   = '\r\n'
+B_CRLF = b'\r\n'
 
 class HttpParser:
     def __init__(self, raw):
@@ -8,11 +10,13 @@ class HttpParser:
         self.version = None
         self.headers = None
         self.body    = None
+        ## Request  : has "method", "path" too
+        ## Response : has "status", "message" too
         self.parser(raw)
 
     def parser(self, raw):
-        info, body = raw.split(CRLF*2, 1)
-        infoline, headers = info.split(CRLF, 1)
+        info, body = bytes.split(raw, B_CRLF*2, 1)
+        infoline, headers = info.decode().split(CRLF, 1)
         self._http_info(infoline)
         self._header_parser(headers)
         self._body_parser(body)
@@ -39,9 +43,15 @@ class HttpParser:
         for line in raw.splitlines():
             key, value = line.split(': ')
             self.headers.update({key:value})
+        if 'Content-Encoding' in self.headers:
+            self.headers['Content-Encoding'] \
+                = [x.strip() for x in self.headers['Content-Encoding'].split(',')]
+        if 'Transfer-Encoding' in self.headers:
+            self.headers['Transfer-Encoding'] \
+                = [x.strip() for x in self.headers['Transfer-Encoding'].split(',')]
 
     def _body_parser(self, raw):
-        self.body = raw
+        self.body = raw.decode()
 
     def print_info(self):
         if   self._is == 'request':
@@ -57,6 +67,9 @@ class HttpParser:
             infoline = "{0} {1} HTTP/{2}".format(self.method, self.path, self.version)
         elif self._is == 'response':
             infoline = "HTTP/{0} {1} {2}".format(self.version, self.status, self.message)
-        raw = infoline + CRLF + CRLF.join([k+': '+v for k, v in self.headers.items()]) \
+        raw = infoline + CRLF \
+            + CRLF.join([
+                k+': '+v if k!='Content-Encoding' and k!='Transfer-Encoding'
+                else k+': '+', '.join(v) for k, v in self.headers.items()]) \
             + CRLF*2 + self.body
         return raw
